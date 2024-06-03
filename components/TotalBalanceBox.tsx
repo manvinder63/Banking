94192e2d6cd48cea7ad8c17eb5ef4ddaa90a0a27
manvinder63 <manvinder1963@outlook.com>
2024-06-03 18:24:31 +1000

    implement header functionality and total balance functionality

---
+++ components/TotalBalanceBox.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { formatAmount } from '@/lib/utils'
+
+const TotalBalanceBox = ({ accounts, totalBanks , totalCurrentBalance} : TotlaBalanceBoxProps) => {
+  return (
+    <section className='total-balance'>
+        <div className='total-balance-chart'>
+            {/* DoughnutChart */}
+
+        </div>
+        <div className='flex flex-col gap-6'>
+            <h2 className='flex flex-col gap-6'>
+              Bank Accounts :  {totalBanks} 
+            </h2>
+            <div className='flex flex-col gap-2'>
+                <p className='total-balance-label'>
+                    Total Current Balance
+                </p>
+                <p className='total-balance-amount flex-center gap-2'>
+                    {formatAmount(totalCurrentBalance)}
+                </p>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default TotalBalanceBox
